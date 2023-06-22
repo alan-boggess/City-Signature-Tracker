34cf58329960fe531dbec9a217f61eb406de7293
faith-grins <alan-boggess@users.noreply.github.com>
2023-06-22 02:01:20 -0500

    Adding basic table functionality.

--- web/city-signature-tracker/src/App.js
+++ web/city-signature-tracker/src/App.js
@@ -1,22 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+function App({ characterStore }) {
+  const [charName, setCharName] = useState('');
+  const [characters, setCharacters] = useState(() => {
+    const json = localStorage.getItem('characters');
+    const savedCharacters = JSON.parse(json);
+    if (savedCharacters) {
+      return savedCharacters;
+    }
+    else return [];
+  });
+
+  let nextId = 0;
+
+  useEffect(() => {
+    const json = JSON.stringify(characters);
+    localStorage.setItem('characters', json);
+  }, [characters]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <center>
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Alignment Merit</th>
+                <th>20 Merits</th>
+                <th>Astral Merit</th>
+                <th>Threads</th>
+              </tr>
+            </thead>
+            <tbody>
+              {characters.map( c => (
+                <tr>
+                  <td>{c.name}</td>
+                  <td>placeholder text</td>
+                </tr>
+                ))
+              }
+            </tbody>
+          </table>
+          <input
+            value={charName}
+            onChange={c => setCharName(c.target.value)}
+          />
+          <button onClick={() => {
+            setCharacters([
+              ...characters,
+              { id: nextId++, name: charName }
+             ]);
+          }}>Add Character</button>
+        </center>
       </header>
     </div>
   );
