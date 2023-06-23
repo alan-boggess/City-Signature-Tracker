a19f6fd5f2021645c00e5148f6cee29416f31cbe
faith-grins <alan-boggess@users.noreply.github.com>
2023-06-22 21:00:41 -0500

    Fill out table with (empty) buttons.

--- web/city-arc-tracker/src/App.js
+++ web/city-arc-tracker/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 
@@ -13,12 +12,24 @@ function App({ characterStore }) {
     else return [];
   });
 
-  let nextId = 0;
+  const [charId, setCharId] = useState(() => {
+    const json = localStorage.getItem('charId');
+    const charId = JSON.parse(json);
+    if (charId) {
+      return charId;
+    }
+    else return characters.length;
+  });
 
   useEffect(() => {
     const json = JSON.stringify(characters);
     localStorage.setItem('characters', json);
   }, [characters]);
+
+  useEffect(() => {
+    const json = JSON.stringify(charId);
+    localStorage.setItem('charId', json);
+  }, [charId]);
 
   return (
     <div className="App">
@@ -39,8 +50,17 @@ function App({ characterStore }) {
             <tbody>
               {characters.map( c => (
                 <tr>
+                  <td>
+                    <button onClick={() => {
+                      setCharacters(characters.filter(char => char.name !== c.name))
+                    }}>X</button>
+                  </td>
+                  <td>{c.id}</td>
                   <td>{c.name}</td>
-                  <td>placeholder text</td>
+                  <td>{LockoutTime(c, c.morality)}</td>
+                  <td>{LockoutTime(c, c.merits)}</td>
+                  <td>{LockoutTime(c, c.astral)}</td>
+                  <td>{LockoutTime(c, c.threads)}</td>
                 </tr>
                 ))
               }
@@ -53,13 +73,25 @@ function App({ characterStore }) {
           <button onClick={() => {
             setCharacters([
               ...characters,
-              { id: nextId++, name: charName }
+              { id: charId, name: charName, alignment: 'hero' }
              ]);
+            setCharId(charId + 1);
           }}>Add Character</button>
         </center>
       </header>
     </div>
   );
+}
+
+function LockoutTime(character, time) {
+  if (time)
+    return time;
+  else {
+    return (
+      <button onClick={() => {
+      }}>Cleared</button>
+    )
+  }
 }
 
 export default App;
